87cfae9d4b599ae3b5fea5c6f07f919f92d39d3f
jorgeacruz <j.alexcruz@gmail.com>
2022-05-18 09:54:10 -0300

    new page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,16 @@
-import { StyleSheet, Text, View, Image , Dimensions, StatusBar, TouchableOpacity} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
 
-const navigation = useNavigation();
+const { height, width } = Dimensions.get('screen');
 
 export default function Home() {
-  
-  const { height, width } = Dimensions.get('window');
-  
-  
-  return (
-    <View style={styles.container}>
-      <StatusBar hidden={true} />
-      <View style={styles.Back}>
-        <Image source={require('../images/homePam.png')} style={{height:height, width:width}}/>
-      </View>
+ return (
+   <View style={styles.container}>
+       <View>
+            <Image source={require('../images/homePam.png')} style={styles.imgBG}/>
+       </View>
 
-      <View style={styles.dataPosition}>
+       <View style={styles.dataPosition}>
         <Text style={styles.dataEvento}>30.05</Text>
         <Text style={styles.anoEvento}>2022</Text>
       </View>
@@ -30,61 +25,64 @@ export default function Home() {
 
       </View>
       
-      
-    </View>
+       
+   </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  },
-  dataPosition: {
-    position:'absolute',
-    left:10,
-    top:10
-  },
-  dataEvento: {
-    color:'#A6A5A5',
-    fontWeight:'bold',
-    fontSize:50
-  },
-  anoEvento: {
-    position:'absolute',
-    top:60,
-    fontWeight:'bold',
-    color:'#A6A5A5',
-    fontSize:30
-  },
-  painelBaixo: {
-    position:'absolute',
-    justifyContent:'center',
-    alignItems:'center',
-    bottom:30
-  },
-  Titulo: {
-    fontSize:40,
-    fontWeight:'bold',
-    paddingBottom:10,
-    color:'#fff'
-  },
-  CronoData: {
-    color:'#fff',
-    fontSize:40,
-    fontWeight:'700',
-    paddingBottom:20,
-  },
-  BtPresenca: {
-    width:300,
-    height:40,
-    borderRadius:5,
-    backgroundColor:'#fff',
-    justifyContent:'center',
-    alignItems:'center'
-  }
-});
+    container: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#fff'
+
+    },
+    imgBG: {
+        height:height,
+        width:width
+    },
+    dataPosition: {
+        position:'absolute',
+        left:10,
+        top:10
+      },
+      dataEvento: {
+        color:'#A6A5A5',
+        fontWeight:'bold',
+        fontSize:50
+      },
+      anoEvento: {
+        position:'absolute',
+        top:60,
+        fontWeight:'bold',
+        color:'#A6A5A5',
+        fontSize:30
+      },
+      painelBaixo: {
+        position:'absolute',
+        justifyContent:'center',
+        alignItems:'center',
+        bottom:30
+      },
+      Titulo: {
+        fontSize:40,
+        fontWeight:'bold',
+        paddingBottom:10,
+        color:'#fff'
+      },
+      CronoData: {
+        color:'#fff',
+        fontSize:40,
+        fontWeight:'700',
+        paddingBottom:20,
+      },
+      BtPresenca: {
+        width:300,
+        height:40,
+        borderRadius:5,
+        backgroundColor:'#fff',
+        justifyContent:'center',
+        alignItems:'center'
+      }
+    });
