0f6887d9ec4e86769b934e4eb0d1ffbdfc76a45d
jorgeacruz <j.alexcruz@gmail.com>
2022-05-18 09:53:10 -0300

    routes

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './src/pages';
-import Confirmeresenca from './src/pages/formPresenca';
 
 const Stack = createNativeStackNavigator();
 
 export default function App(){
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="ConfirmePresenca" component={ConfirmePresenca} />
+      <Stack.Navigator>
+
+        <Stack.Screen name='Home' component={Home} 
+        options={{ headerShown:false}}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   )
